04fb808735ceeadc71c9814d27c000bdd5fb57a5
adhikari-arpan <adhikariarpan2063@gmail.com>
2024-10-09 11:17:53 +0545

    Update

--- script.js
+++ script.js
@@ -45,6 +45,30 @@ document.getElementById('back-to-top').addEventListener('click', function() {
     window.scrollTo(0, 0);
 });
 
+//Form Submission
+    document.getElementById('contactForm').addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent the default form submission
+
+        const formData = new FormData(this);
+
+        fetch(this.action, {
+            method: this.method,
+            body: formData,
+            headers: {
+                'Accept': 'application/json'
+            }
+        }).then(response => {
+            if (response.ok) {
+                alert('Thank you for contacting me! I will reach back to you soon.');
+                this.reset(); // Reset the form after successful submission
+            } else {
+                alert('There was a problem submitting your info.');
+            }
+        }).catch(error => {
+            alert('There was a problem submitting your info.');
+        });
+    });
+
 // Fade in animation to sections
 document.addEventListener('scroll', function() {
     const sections = document.querySelectorAll('section');
@@ -54,3 +78,14 @@ document.addEventListener('scroll', function() {
         }
     });
 });
+
+//Nav Bar for Mobile Screen
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('nav ul');
+  
+    hamburger.addEventListener('click', function() {
+      navMenu.classList.toggle('show');
+    });
+  });
+  
